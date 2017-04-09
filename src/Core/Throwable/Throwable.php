779ef3c07123f11ec665f71b0b69669907fdb209
toy-php <seymus007@gmail.com>
2017-04-09 16:50:27 +0300

    Update

--- src/Core/Throwable/Throwable.php
+++ src/Core/Throwable/Throwable.php
@@ -14,33 +14,13 @@ class Throwable
         $this->application = $application;
     }
 
-    public function getChunkCode($fileName, $startRow, $numRows)
-    {
-        if (!file_exists($fileName)) {
-            return false;
-        }
-        $arrayLines = (array)file($fileName);
-        array_unshift($arrayLines, '');
-        $startRow = (count($arrayLines) > $numRows)
-            ? ($startRow - ceil($numRows / 2)) + 1
-            : 1;
-        return array_slice($arrayLines, $startRow, $numRows, true);
-    }
-
     public function handle(\Throwable $throwable)
     {
-        $templateConfig['dir'] = __DIR__ . '/template/';
-        $templateConfig['functions'] = [
-            'getChunkCode' => [$this, 'getChunkCode'],
-            'clean' => function ($string) {
-                return filter_var($string, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            },
-        ];
-        $template = $this->application->getTemplate($templateConfig);
+        $template = $this->application->getTemplate(__DIR__ . '/template/');
         $assets = str_replace($_SERVER['DOCUMENT_ROOT'], "", __DIR__) . '/template/';
         echo $template->render(
             'error',
-            [
+            new ViewModel([
                 'assets' => $assets,
                 'request' => $this->application->getRequest(),
                 'message' => $throwable->getMessage(),
@@ -49,7 +29,7 @@ class Throwable
                 'line' => $throwable->getLine(),
                 'previous' => $throwable->getPrevious(),
                 'trace' => $throwable->getTrace()
-            ]
+            ])
         );
     }
 }
---
+++ src/Core/Throwable/ViewModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Core\Throwable;
+
+class ViewModel extends \Core\Template\ViewModel
+{
+
+    public function getChunkCode($fileName, $startRow, $numRows)
+    {
+        if (!file_exists($fileName)) {
+            return false;
+        }
+        $arrayLines = (array)file($fileName);
+        array_unshift($arrayLines, '');
+        $startRow = (count($arrayLines) > $numRows)
+            ? ($startRow - ceil($numRows / 2)) + 1
+            : 1;
+        return array_slice($arrayLines, $startRow, $numRows, true);
+    }
+
+    public function clean($string)
+    {
+        return filter_var($string, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    }
+}
